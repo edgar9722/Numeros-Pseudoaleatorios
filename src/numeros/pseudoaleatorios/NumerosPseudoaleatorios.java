b247dfdfa42e8eb1083eea454a00a9ad8937133a
h4ckerv2 <sabrisjuan@hotmail.com>
2019-04-08 11:13:43 -0500

    Prueba

--- src/numeros/pseudoaleatorios/NumerosPseudoaleatorios.java
+++ src/numeros/pseudoaleatorios/NumerosPseudoaleatorios.java
@@ -5,6 +5,8 @@ public class NumerosPseudoaleatorios {
     public static void main(String[] args) {
         VentanaOpciones principal = new VentanaOpciones();
         principal.setVisible(true);
+        int hola = 4;
+        System.out.println(hola);
     }
     
 }
